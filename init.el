694fcd382aa3fdb6a2d4dade737e90d74e946c69
cam <cameronayres123@gmail.com>
2025-03-14 20:32:46 +0000

    Misc changes.

--- init.el
+++ init.el
@@ -2,20 +2,20 @@
 
 (defvar bootstrap-version)
 (let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 6))
+	 (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+	(bootstrap-version 6))
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
-	(url-retrieve-synchronously
-	 "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-	 'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
+	  (url-retrieve-synchronously
+	   "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+	   'silent 'inhibit-cookies)
+	(goto-char (point-max))
+	(eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 
 (straight-use-package 'use-package)
 (setq straight-use-package-by-default t
-      use-package-always-ensure nil)
+	use-package-always-ensure nil)
 
 (defun display-startup-echo-area-message nil
   "I don't want gnu's stuff at startup, I want my stuff."
@@ -28,14 +28,14 @@
 (use-package paredit
   :defer nil
   :hook ((scheme-mode     . enable-paredit-mode)
-	 (emacs-lisp-mode . enable-paredit-mode)
-	 (clojure-mode    . enable-paredit-mode)))
+	   (emacs-lisp-mode . enable-paredit-mode)
+	   (clojure-mode    . enable-paredit-mode)))
 
 (use-package rainbow-delimiters
   :defer nil
   :hook ((scheme-mode     . rainbow-delimiters-mode)
-	 (emacs-lisp-mode . rainbow-delimiters-mode)
-	 (clojure-mode    . rainbow-delimiters-mode)))
+	   (emacs-lisp-mode . rainbow-delimiters-mode)
+	   (clojure-mode    . rainbow-delimiters-mode)))
 
 (use-package geiser
   :straight t
@@ -57,23 +57,23 @@
 (use-package lean4-mode
   :commands lean4-mode
   :straight (lean4-mode :type git
-			:host github
-			:repo "leanprover-community/lean4-mode"
-			:files ("*.el" "data"))
+			  :host github
+			  :repo "leanprover-community/lean4-mode"
+			  :files ("*.el" "data"))
   :config
   (add-to-list 'exec-path (concat (getenv "HOME")
-				  "/.elan/bin"))
+				    "/.elan/bin"))
   (setenv "PATH" (concat (concat (getenv "HOME") "/.elan/bin" ":")
-			 (getenv "PATH"))))
+			   (getenv "PATH"))))
 
 (use-package rust-mode
   :config
   (let ((cargo-path)) (concat (getenv "HOME")
-			      "/cargo/bin")
+				"/cargo/bin")
 
-       (setenv "PATH" (concat (getenv "PATH")
-			      ":" cargo-path))
-       (add-to-list 'exec-path cargo-path)))
+	 (setenv "PATH" (concat (getenv "PATH")
+				":" cargo-path))
+	 (add-to-list 'exec-path cargo-path)))
 
 (use-package rustic
   :hook (rustic-mode . lsp-mode)
@@ -82,9 +82,9 @@
 
 (use-package haskell-mode
   :hook ((haskell-mode          . interactive-haskell-mode)
-	 (haskell-mode          . lsp)
-	 (haskell-literate-mode . interactive-haskell-mode)
-	 (haskell-literate-mode . lsp))
+	   (haskell-mode          . lsp)
+	   (haskell-literate-mode . interactive-haskell-mode)
+	   (haskell-literate-mode . lsp))
   :bind
   (:map haskell-mode-map ("C-c C-c" . haskell-compile))
   (:map haskell-cabal-mode-map ("C-c C-c" . haskell-compile))
@@ -96,11 +96,13 @@
 (use-package lsp-haskell
   :after (haskell-mode))
 
-(use-package lsp-java
-  :defer t
-  :hook ((java-mode . lsp-mode)
-	 (lsp-mode  . lsp-java-mode))
-  :after (:all lsp-mode magit))
+;; (use-package lsp-java
+;;   :defer t
+;;   :hook ((java-mode . lsp-mode)
+;; 	   (lsp-mode  . lsp-java-mode))
+;;   :after (:all lsp-mode magit)
+;;   :config
+;;   (setq lsp-enable-snippet nil))
 
 (defun cam/java-run-in-buffer (&optional filename)
   "Just opens a buffer, and runs a Java program in it.
@@ -111,18 +113,18 @@ If FILENAME isn't given, then it will just run the current
 buffer."
   (interactive)
   (let ((java-run-buffer-name "*cam/java-run*")
-	(java-buffer-name (if filename
-			      filename
-			    (buffer-name))))
+	  (java-buffer-name (if filename
+				filename
+			      (buffer-name))))
 
     (switch-to-buffer-other-window java-run-buffer-name)
     (erase-buffer)
     (start-process "*java-running-process*" java-run-buffer-name
-		   "java" java-buffer-name)
+		     "java" java-buffer-name)
     (other-window 1)))
 
 (add-hook 'java-mode-hook
-	  (lambda () (local-set-key (kbd "C-c C-r") #'cam/java-run-in-buffer)))
+	    (lambda () (local-set-key (kbd "C-c C-r") #'cam/java-run-in-buffer)))
 
 (use-package magit)
 
@@ -151,86 +153,86 @@ just the articles. This marks them as read."
   :bind ("C-c e" . elfeed)
   :config
   (setq elfeed-feeds
-	'(("https://planet.emacslife.com/atom.xml" blog emacs)
-	  ("https://summeremacs.github.io/posts/index.xml" blog emacs)
-	  ("https://xkcd.com/rss.xml" comic)
-	  ("https://www.smbc-comics.com/comic/rss" comic)
-	  ("https://www.monkeyuser.com/index.xml" comic)
-	  ("https://archlinux.org/feeds/news/" arch linux tech)
-	  ("https://wolfgirl.dev/blog/rss.xml" blog tech prog)
-	  ("https://izzys.casa/index.xml" blog tech prog)
-	  ("https://faultlore.com/blah/rss.xml" blog tech prog)
-	  ("https://welltypedwit.ch/rss.xml" tech blog)
-	  ("https://feeds.bbci.co.uk/news/bbcindepth/rss.xml" news bbc)
-	  ("https://www.quantamagazine.org/feed/" sci news)
-	  ("https://feeds.libsyn.com/499093/rss" tech podcast)
-	  ("http://hackaday.libsyn.com/rss" tech podcast)
-	  ("https://rustacean-station.org/podcast.rss" tech prog podcast)
-	  ("https://risky.biz/feeds/risky-business-news/" tech security podcast)
-	  ;; Leadhead
-	  ("https://www.youtube.com/feeds/videos.xml?channel_id=UC3_kehZbfRz-KrjXIqeIiPw" blog video)
-	  ;; Helluva Boss
-	  ("https://www.youtube.com/feeds/videos.xml?channel_id=UCzfyYtgvkx5mLy8nlLlayYg" video show)
-	  ;; oliSUNvia
-	  ("https://www.youtube.com/feeds/videos.xml?channel_id=UCVHxJghKAB_kA_5LMM8MD3w" video phil)
-	  ;; Wendigoon
-	  ("https://www.youtube.com/feeds/videos.xml?channel_id=UC3cpN6gcJQqcCM6mxRUo_dA" video spooky)
-	  ;; ABSTRACT
-	  ("https://www.youtube.com/feeds/videos.xml?channel_id=UCIPfjC8FVLdul4-35JekB1g" video spooky)
-	  ;; SOG
-	  ("https://www.youtube.com/feeds/videos.xml?channel_id=UCtMVHI3AJD4Qk4hcbZnI9ZQ" video blog))))
+	  '(("https://planet.emacslife.com/atom.xml" blog emacs)
+	    ("https://summeremacs.github.io/posts/index.xml" blog emacs)
+	    ("https://xkcd.com/rss.xml" comic)
+	    ("https://www.smbc-comics.com/comic/rss" comic)
+	    ("https://www.monkeyuser.com/index.xml" comic)
+	    ("https://archlinux.org/feeds/news/" arch linux tech)
+	    ("https://wolfgirl.dev/blog/rss.xml" blog tech prog)
+	    ("https://izzys.casa/index.xml" blog tech prog)
+	    ("https://faultlore.com/blah/rss.xml" blog tech prog)
+	    ("https://welltypedwit.ch/rss.xml" tech blog)
+	    ("https://feeds.bbci.co.uk/news/bbcindepth/rss.xml" news bbc)
+	    ("https://www.quantamagazine.org/feed/" sci news)
+	    ("https://feeds.libsyn.com/499093/rss" tech podcast)
+	    ("http://hackaday.libsyn.com/rss" tech podcast)
+	    ("https://rustacean-station.org/podcast.rss" tech prog podcast)
+	    ("https://risky.biz/feeds/risky-business-news/" tech security podcast)
+	    ;; Leadhead
+	    ("https://www.youtube.com/feeds/videos.xml?channel_id=UC3_kehZbfRz-KrjXIqeIiPw" blog video)
+	    ;; Helluva Boss
+	    ("https://www.youtube.com/feeds/videos.xml?channel_id=UCzfyYtgvkx5mLy8nlLlayYg" video show)
+	    ;; oliSUNvia
+	    ("https://www.youtube.com/feeds/videos.xml?channel_id=UCVHxJghKAB_kA_5LMM8MD3w" video phil)
+	    ;; Wendigoon
+	    ("https://www.youtube.com/feeds/videos.xml?channel_id=UC3cpN6gcJQqcCM6mxRUo_dA" video spooky)
+	    ;; ABSTRACT
+	    ("https://www.youtube.com/feeds/videos.xml?channel_id=UCIPfjC8FVLdul4-35JekB1g" video spooky)
+	    ;; SOG
+	    ("https://www.youtube.com/feeds/videos.xml?channel_id=UCtMVHI3AJD4Qk4hcbZnI9ZQ" video blog))))
 
 (setq erc-server "irc.libera.chat"
-      etc-track-shorten-start 8
-      erc-kill-buffer-on-part t
-      erc-auto-query 'bury
-      erc-user-full-name "cam a."
-      erc-nick "vibe876")
+	etc-track-shorten-start 8
+	erc-kill-buffer-on-part t
+	erc-auto-query 'bury
+	erc-user-full-name "cam a."
+	erc-nick "vibe876")
 
 (use-package emms
   :defer t
   :config
   (setq emms-player-list '(emms-player-mpv)
-	emms-source-file-default-directory "/home/cam/Music/music"))
+	  emms-source-file-default-directory "/home/cam/Music/music"))
 
 (emms-all)
 
 (setq org-directory (concat (getenv "HOME") "/Documents/Org")
-      org-agenda-files (directory-files-recursively org-directory
-						  (rx bol
-						      (one-or-more (or lower-case ?-))
-						      ".org" eol)))
+	org-agenda-files (directory-files-recursively org-directory
+						    (rx bol
+							(one-or-more (or lower-case ?-))
+							".org" eol)))
 
 (global-set-key (kbd "C-c a") 'org-agenda)
 
 (setq org-agenda-directory (concat org-directory "/Agenda")
-      org-agenda-work-file (concat org-agenda-directory "/work.org")
-      org-agenda-social-file (concat org-agenda-directory "/social.org")
-      org-agenda-personal-file (concat org-agenda-directory "/personal.org")
+	org-agenda-work-file (concat org-agenda-directory "/work.org")
+	org-agenda-social-file (concat org-agenda-directory "/social.org")
+	org-agenda-personal-file (concat org-agenda-directory "/personal.org")
 
-      org-capture-templates
-      '(("u" "Uni Stuff" entry (file+headline org-agenda-work-file "Uni")
+	org-capture-templates
+	'(("u" "Uni Stuff" entry (file+headline org-agenda-work-file "Uni")
+	   "* TODO [#C] %?\nSCHEDULED: %t")
+	("s" "Social Stuff" entry (file+headline org-agenda-social-file "Misc")
 	 "* TODO [#C] %?\nSCHEDULED: %t")
-      ("s" "Social Stuff" entry (file+headline org-agenda-social-file "Misc")
-       "* TODO [#C] %?\nSCHEDULED: %t")
-      ("p" "Personal" entry (file+headline org-agenda-personal-file "Stuff ToDo")
-       "* TODO [#C] %?\nSCHEDULED: %t")))
+	("p" "Personal" entry (file+headline org-agenda-personal-file "Stuff ToDo")
+	 "* TODO [#C] %?\nSCHEDULED: %t")))
 
 (global-set-key (kbd "C-c c") 'org-capture)
 
 (setq org-latex-listings 'minted
-      org-latex-pdf-process '("pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
-			      "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
-			      "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f")
-
-      org-html-validation-link nil
-      org-export-with-author nil
-      org-export-with-toc nil)
+	org-latex-pdf-process '("pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+				"pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+				"pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f")
+    
+	org-html-validation-link nil
+	org-export-with-author nil
+	org-export-with-toc nil)
 
 (use-package ef-themes
   :defer nil
   :config
-  (load-theme 'ef-tritanopia-dark t))
+  (load-theme 'ef-night t))
 
 (tool-bar-mode -1)
 (menu-bar-mode -1)
@@ -238,5 +240,8 @@ just the articles. This marks them as read."
 
 ;(defalias 'yes-or-no-p 'y-o-n-p)
 
+(use-package sequed
+  :defer t)
+
 (load-file (concat (getenv "HOME")
-		   "/.emacs.d/secrets.el"))
+		     "/.emacs.d/secrets.el"))
